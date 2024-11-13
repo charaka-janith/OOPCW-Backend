4792c37d0e11f11816862557d1beb0c78d7eb295
Charaka Janith <charakaj@lankatiles.com>
2024-11-13 15:50:25 +0530

    init n running

---
+++ src/main/java/iit/y3/oopcw/OopcwApplication.java
@@ -0,0 +1,12 @@
+package iit.y3.oopcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopcwApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OopcwApplication.class, args);
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/iit/y3/oopcw/OopcwApplicationTests.java
@@ -0,0 +1,13 @@
+package iit.y3.oopcw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopcwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
